80d9f49e56d6ee31f3522e49d5b0058dc491f45d
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2022-10-14 17:14:16 -0400

    remove version constrain from provider

--- providers.tf
+++ providers.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.0"
     }
     http = {
         source = "hashicorp/http"
-        version = "~> 3.0.0"
     }
   }
 }
